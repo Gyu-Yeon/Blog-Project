dedbe5e5dcc198c31343128b1fda02f0e15bf449
Gyu-Yeon <idsopy@nate.com>
2024-03-05 22:22:43 +0900

    main page6

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,9 +1,16 @@
-import ButtonAppBar from "../AppBar";
-
 export default function Notes() {
+  const data = [{ name: "Johnny" }, { name: "Jason" }, { name: "Paul" }];
   return (
     <div className="notes">
-      <div className="notes-container mt-10 m-auto">s</div>
+      <div className="notes-container mt-10 m-auto d-flex">
+        {data.map((item, i) => {
+          return (
+            <div className="notes-item" key={i}>
+              <span>{item.name}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
